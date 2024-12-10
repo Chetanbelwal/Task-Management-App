fb4787dc3d992ebc2f795e23e385a5a4a3fc94d6
Chetan Belwal <chetan.belwal13@gmail.com>
2024-12-11 00:32:51 +0530

    middleware added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.json({
-        "message":"Hy i am loading"
-    })
-})
+// Load environment variables
+dotenv.config({ path: "./config/config.env" });
+
+// Define CORS options
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+// Apply CORS middleware with options
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import app from "./app.js"
+const PORT = process.env.PORT || 5000
 
-app.listen(4000, ()=>{
-    console.log(`server is running at port 4000`);
+app.listen(PORT, ()=>{
+    console.log(`server is running at port ${PORT}`);
 })
