812781057a386dd816503c16577d6b3607a524b5
Chetan Belwal <chetan.belwal13@gmail.com>
2025-01-28 23:52:56 +0530

    keeping secure as true

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken = (message, user, res, statusCode) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      sameSite: "none",
+      secure: true,
     };
     res.status(statusCode).cookie("token", token, options).json({
       success: true,
